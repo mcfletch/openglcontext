9215ca9594269ceebd20591ecb13fffa39f351b0
Mike C. Fletcher <mcfletch@vrplumber.com>
2009-02-28 00:13:17 -0500

    Rewrite to use triangles instead of quads

--- OpenGLContext/drawcube.py
+++ OpenGLContext/drawcube.py
@@ -9,15 +9,17 @@ This version was taken from the NeHe tutorials,
 to replace the original which did not include
 texture coordinate information.
 """
-
 from OpenGL.GL import *
+
 def drawCube():
 	"""Draw a cube 2,2,2 units centered around the origin"""
 	# draw six faces of a cube
-	glBegin(GL_QUADS);
+	glBegin(GL_TRIANGLES);
 	glNormal3f( 0.0, 0.0, 1.0)
 	glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0,  1.0);
 	glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0,  1.0);
+	glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0,  1.0);
+	glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0,  1.0);
 	glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0,  1.0);
 	glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0,  1.0);
 
@@ -25,11 +27,15 @@ def drawCube():
 	glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0, -1.0);
 	glTexCoord2f(1.0, 1.0); glVertex3f(-1.0,  1.0, -1.0);
 	glTexCoord2f(0.0, 1.0); glVertex3f( 1.0,  1.0, -1.0);
+	glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0, -1.0);
+	glTexCoord2f(0.0, 1.0); glVertex3f( 1.0,  1.0, -1.0);
 	glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0, -1.0);
 
 	glNormal3f( 0.0, 1.0, 0.0)
 	glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0, -1.0);
 	glTexCoord2f(0.0, 0.0); glVertex3f(-1.0,  1.0,  1.0);
+	glTexCoord2f(1.0, 0.0); glVertex3f( 1.0,  1.0,  1.0);
+	glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0, -1.0);
 	glTexCoord2f(1.0, 0.0); glVertex3f( 1.0,  1.0,  1.0);
 	glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0, -1.0);
 
@@ -37,17 +43,23 @@ def drawCube():
 	glTexCoord2f(1.0, 1.0); glVertex3f(-1.0, -1.0, -1.0);
 	glTexCoord2f(0.0, 1.0); glVertex3f( 1.0, -1.0, -1.0);
 	glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0,  1.0);
+	glTexCoord2f(1.0, 1.0); glVertex3f(-1.0, -1.0, -1.0);
+	glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0,  1.0);
 	glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0,  1.0);
 
 	glNormal3f( 1.0, 0.0, 0.0)
 	glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0, -1.0);
 	glTexCoord2f(1.0, 1.0); glVertex3f( 1.0,  1.0, -1.0);
 	glTexCoord2f(0.0, 1.0); glVertex3f( 1.0,  1.0,  1.0);
+	glTexCoord2f(1.0, 0.0); glVertex3f( 1.0, -1.0, -1.0);
+	glTexCoord2f(0.0, 1.0); glVertex3f( 1.0,  1.0,  1.0);
 	glTexCoord2f(0.0, 0.0); glVertex3f( 1.0, -1.0,  1.0);
 
 	glNormal3f(-1.0, 0.0, 0.0)
 	glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0, -1.0);
 	glTexCoord2f(1.0, 0.0); glVertex3f(-1.0, -1.0,  1.0);
+	glTexCoord2f(1.0, 1.0); glVertex3f(-1.0,  1.0,  1.0);
+	glTexCoord2f(0.0, 0.0); glVertex3f(-1.0, -1.0, -1.0);
 	glTexCoord2f(1.0, 1.0); glVertex3f(-1.0,  1.0,  1.0);
 	glTexCoord2f(0.0, 1.0); glVertex3f(-1.0,  1.0, -1.0);
 	glEnd()
