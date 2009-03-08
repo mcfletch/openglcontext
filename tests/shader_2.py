957539a5a1e5d732544821c8d1f58c440a077558
Mike C. Fletcher <mcfletch@sturm>
2009-03-08 17:18:38 -0400

    First three shader samples

--- tests/shader_2.py
+++ tests/shader_2.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-'''Draw simplest possible shader-based geometry
+'''Shader sample-code for OpenGLContext
 '''
 from OpenGLContext import testingcontext
 BaseContext, MainFunction = testingcontext.getInteractive()
@@ -9,29 +9,33 @@ from OpenGLContext.arrays import *
 from OpenGLContext.scenegraph.shaders import compileProgram, glUseProgram
 
 class TestContext( BaseContext ):
-	"""Creates a simple vertex shader..."""
+	"""This shader just passes gl_Color from an input array to 
+	the fragment shader, which interpolates the values across the 
+	face (a "varying" data type).
+	"""
 	
 	def OnInit( self ):
 		self.shader = compileProgram(
 			'''void main() {
 				gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+				gl_FrontColor = gl_Color;
 			}''',
 			'''void main() {
-				gl_FragColor = vec4( 0, 1, 0, 1 );
+				gl_FragColor = gl_Color;
 			}''',
 		)
 		self.vbo = vbo.VBO(
 			array( [
-				[  0, 1, 0 ],
-				[ -1,-1, 0 ],
-				[  1,-1, 0 ],
+				[  0, 1, 0,  0,1,0 ],
+				[ -1,-1, 0,  1,1,0 ],
+				[  1,-1, 0,  0,1,1 ],
 				
-				[  2,-1, 0 ],
-				[  4,-1, 0 ],
-				[  4, 1, 0 ],
-				[  2,-1, 0 ],
-				[  4, 1, 0 ],
-				[  2, 1, 0 ],
+				[  2,-1, 0,  1,0,0 ],
+				[  4,-1, 0,  0,1,0 ],
+				[  4, 1, 0,  0,0,1 ],
+				[  2,-1, 0,  1,0,0 ],
+				[  4, 1, 0,  0,0,1 ],
+				[  2, 1, 0,  0,1,1 ],
 			],'f')
 		)
 	
@@ -43,7 +47,9 @@ class TestContext( BaseContext ):
 			self.vbo.bind()
 			try:
 				glEnableClientState(GL_VERTEX_ARRAY);
-				glVertexPointerf( self.vbo )
+				glEnableClientState(GL_COLOR_ARRAY);
+				glVertexPointer(3, GL_FLOAT, 24, self.vbo )
+				glColorPointer(3, GL_FLOAT, 24, self.vbo+12 )
 				glDrawArrays(GL_TRIANGLES, 0, 9)
 			finally:
 				self.vbo.unbind()
---
+++ tests/shader_3.py
@@ -0,0 +1,84 @@
+#! /usr/bin/env python
+'''Shader sample-code for OpenGLContext
+'''
+from OpenGLContext import testingcontext
+BaseContext, MainFunction = testingcontext.getInteractive()
+from OpenGL.GL import *
+from OpenGL.arrays import vbo
+from OpenGLContext.arrays import *
+from OpenGLContext.scenegraph.shaders import compileProgram, glUseProgram
+
+class TestContext( BaseContext ):
+	"""This shader adds a simple linear fog to the shader
+	
+	Shows use of uniforms, and a few simple calculations 
+	within the vertex shader...
+	"""
+	UNIFORM_VALUES = (
+		(glUniform1f,'end_fog',(12,)),
+		(glUniform4f,'fog_color',(0,0,0,1)),
+	)
+	def OnInit( self ):
+		self.shader = compileProgram(
+			'''
+			uniform float end_fog;
+			uniform vec4 fog_color;
+			void main() {
+				float fog; // amount of fog to apply
+				float fog_coord; // distance for fog calculation...
+				gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+				// ecPosition is the eye-coordinate position...
+				
+				fog_coord = abs(gl_Position.z);
+				fog_coord = clamp( fog_coord, 0.0, end_fog);
+				fog = (end_fog - fog_coord)/end_fog;
+				fog = clamp( fog, 0.0, 1.0);
+				gl_FrontColor = mix(fog_color, gl_Color, fog);
+			}''',
+			'''void main() {
+				gl_FragColor = gl_Color;
+			}''',
+		)
+		self.vbo = vbo.VBO(
+			array( [
+				[  0, 1, 0,  0,1,0 ],
+				[ -1,-1, 0,  1,1,0 ],
+				[  1,-1, 0,  0,1,1 ],
+				
+				[  2,-1, 0,  1,0,0 ],
+				[  4,-1, 0,  0,1,0 ],
+				[  4, 1, 0,  0,0,1 ],
+				[  2,-1, 0,  1,0,0 ],
+				[  4, 1, 0,  0,0,1 ],
+				[  2, 1, 0,  0,1,1 ],
+			],'f')
+		)
+		self.UNIFORM_LOCATIONS = {}
+		for func,var,values in self.UNIFORM_VALUES:
+			loc = glGetUniformLocation( self.shader, var )
+			self.UNIFORM_LOCATIONS[var] = loc
+	
+	def Render( self, mode = 0):
+		"""Render the geometry for the scene."""
+		BaseContext.Render( self, mode )
+		glUseProgram(self.shader)
+		for func,var,values in self.UNIFORM_VALUES:
+			loc = self.UNIFORM_LOCATIONS[var]
+			func( loc, *values )
+		try:
+			self.vbo.bind()
+			try:
+				glEnableClientState(GL_VERTEX_ARRAY);
+				glEnableClientState(GL_COLOR_ARRAY);
+				glVertexPointer(3, GL_FLOAT, 24, self.vbo )
+				glColorPointer(3, GL_FLOAT, 24, self.vbo+12 )
+				glDrawArrays(GL_TRIANGLES, 0, 9)
+			finally:
+				self.vbo.unbind()
+		finally:
+			glUseProgram( 0 )
+		
+
+if __name__ == "__main__":
+	MainFunction ( TestContext)
+
