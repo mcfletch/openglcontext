2513d87989ac70e9585442fae42aa7e75f490faa
Mike C. Fletcher <mcfletch@sturm>
2009-03-26 13:28:43 -0400

    Add the leak-debugging code

---
+++ OpenGLContext/debug/leaks.py
@@ -0,0 +1,13 @@
+"""Utility mechanism to watch for memory leaks over time"""
+import gc
+
+whole_set = {}
+def init():
+	for object in gc.get_objects():
+		whole_set[id(object)] = True 
+
+def delta():
+	for object in gc.get_objects():
+		if not whole_set.get( id(object)):
+			print 'new', type( object ), object
+		whole_set[id(object)] = True 
