038e435c4fcea9402b2b29cb13a117f40f15a523
Mike C. Fletcher <mcfletch@sturm>
2009-04-11 13:37:58 -0400

    Texture atlas tests as proper unit tests

---
+++ OpenGLContext/atlas.py
@@ -0,0 +1,184 @@
+"""Texture atlas implementation"""
+import math 
+from OpenGL.GL import glGetIntegerv, GL_MAX_TEXTURE_SIZE
+from OpenGLContext.arrays import zeros, array, dot
+
+class _Strip( object ):
+	"""Strip within the atlas which takes particular set of images"""
+	def __init__( self, atlas, height, yoffset ):
+		"""Sets up the strip to receive images"""
+		self.atlas = atlas
+		self.height = height 
+		self.width = 0
+		self.full = False
+		self.yoffset = yoffset
+		self.maps = []
+	def add( self, array ):
+		"""Add the given numpy array to our atlas' image"""
+		local = self.atlas.local( )
+		x,y,d = array.shape
+		assert local.shape[0] - (self.width+x) >= 0
+		assert local.shape[1] - (self.yoffset+y) >= 0
+		local[ self.width:self.width+x,self.yoffset:self.yoffset+y,:] = array 
+		array = local[ self.width:self.width+x,self.yoffset:self.yoffset+y,:]
+		# array now points to the value as a sub-array
+		offset = (self.width,self.yoffset)
+		self.width += x
+		if self.width >= local.shape[0]:
+			self.full = True 
+		map = Map( self.atlas, offset, (x,y), array )
+		return map
+
+class AtlasError( Exception ):
+	"""Raised when we can't/shouldn't append to this atlas"""
+	
+
+class Atlas( object ):
+	"""Collection of textures packed into a single texture
+	
+	Texture atlases allow the rendering engine to reduce the 
+	number of state-changes which occur during the rendering 
+	process.  They pack a large number of small textures into 
+	a single large texture, producing offset/scale matrices to 
+	use for modifying texture coordinates to map from the
+	original to the packed coordinates.
+	"""
+	_local = None
+	_size = None
+	def __init__( self, components=4, dataType='B', max_size=4096 ):
+		self.components = components
+		self.dataType = dataType
+		self.strips = []
+		self.max_size = max_size
+	
+	def add( self, array ):
+		"""Insert a numpy array of values as a sub-texture
+		
+		Has to find a place within the atlas to insert the 
+		sub-texture and then return the offset/scale factors...
+		"""
+		local = self.local()
+		max_x,max_y,components = local.shape 
+		x,y,d = array.shape 
+		assert d == components, """Adding texture to atlas with different number of components"""
+		strip = self.choose_strip( max_x, max_y, x, y )
+		return strip.add( array )
+	def choose_strip( self, max_x,max_y, x,y ):
+		"""Find the strip to which we should be added"""
+		candidates = [ 
+			s 
+			for s in self.strips 
+			if (
+				not s.full and 
+				s.height >= y and 
+				(s.width + x <= max_x)
+			) 
+		]
+		for strip in candidates:
+			if strip.height == y:
+				return strip 
+		for strip in candidates:
+			strip_mag = math.floor(math.log( strip.height, 2 ))
+			img_mag = math.floor(math.log( y, 2 ))
+			if strip_mag == img_mag:
+				return strip 
+		if self.strips:
+			last = self.strips[-1]
+			current_height = last.yoffset + last.height 
+		else:
+			current_height = 0
+		if current_height + y > max_y:
+			raise AtlasError( """Insufficient space to store in this atlas""" )
+		strip = _Strip( self, height=y, yoffset= current_height )
+		self.strips.append( strip )
+		return strip
+	
+	def size( self ):
+		if self._size is None:
+			x = y = self.max_size
+			self._size = (x,y,self.components)
+		return self._size
+	def local( self ):
+		"""Set up our local array for storage"""
+		if self._local is None:
+			self._local = zeros( self.size(), self.dataType )
+		return self._local
+	
+	def render( self, mode=None ):
+		"""Render this texture to the context"""
+		local = self.local( )
+		return local
+	def bind( self, mode=None ):
+		"""Bind this texture for use on mode"""
+		# need to check each map for dirty status 
+		# update any dirty maps 
+		# then do a regular bind with our texture...
+
+class Map( object ):
+	"""Object representing a sub-texture within a texture atlas"""
+	_matrix = None
+	_uploaded = False
+	def __init__( self, atlas, offset, size, array ):
+		self.atlas = atlas 
+		self.offset = offset 
+		self.size = size
+		self.array = array
+	def matrix( self ):
+		"""Calculate a 3x3 transform matrix for texcoords"""
+		if self._matrix is None:
+			# translate by self.offset/atlas.size
+			# scale by self.size/atlas.size
+			x,y = self.offset
+			translate = array([
+				[1,0,0],
+				[0,1,0],
+				[x,y,1],
+			],'f')
+			x,y = self.size 
+			tx,ty,d = self.atlas.size()
+			scale = array([
+				[x/float(tx),0,0],
+				[0,y/float(tx),0],
+				[0,0,1],
+			],'f')
+			self._matrix = dot( scale, translate )
+		return self._matrix
+		
+	def update( self, array ):
+		"""Update our texture with the new data in array"""
+		self._uploaded = False 
+		assert array.size == self.array.size 
+		self.array = array 
+
+class AtlasManager( object ):
+	"""Collection of atlases within the renderer"""
+	def __init__( self, max_size=4096, max_child_size=128 ):
+		self.components = {}
+		self.max_size = max_size
+		self.max_child_size = max_child_size
+	def add( self, array ):
+		"""Add the given array to the texture atlas"""
+		x,y,d = array.shape
+		if x > self.max_child_size:
+			raise AtlasError( """X size (%s) > %s"""%( x,self.max_child_size ) )
+		if y > self.max_child_size:
+			raise AtlasError( """Y size (%s) > %s"""%( y,self.max_child_size ) )
+		atlases = self.components.setdefault( d, [])
+		for atlas in atlases:
+			try:
+				return atlas.add( array )
+			except AtlasError, err:
+				pass 
+		atlas = Atlas( d, max_size=self.max_size or self.calculate_max_size() )
+		atlases.append( atlas )
+		return atlas.add( array )
+	_MAX_MAX_SIZE = 4096
+	def calculate_max_size( self ):
+		"""Calculate the maximum size of a texture
+		
+		Note that this might, for instance, assume a
+		single-component texture or some similarly inappropriate
+		value...
+		"""
+		self.max_size = min( (glGetIntegerv(GL_MAX_TEXTURE_SIZE), self._MAX_MAX_SIZE) )
+		return self.max_size
---
+++ OpenGLContext/tests/test_atlas.py
@@ -0,0 +1,43 @@
+from OpenGLContext import atlas 
+from OpenGLContext.arrays import zeros, array, allclose, dot
+import unittest
+
+class TestAtlas( unittest.TestCase ):
+	"""Tests for generic atlas algorithms"""
+	def setUp( self ):
+		self.atlasManager = atlas.AtlasManager( max_size=256 )
+	
+	def test_assignment( self ):
+		"""Test that assignments go to the correct strips"""
+		map = self.atlasManager.add( zeros( (64,64,4),'B' ) )
+		assert len(self.atlasManager.components) == 1
+		assert len(self.atlasManager.components[4]) == 1
+		atlas = self.atlasManager.components[4][0]
+		assert map.atlas is atlas
+		assert len(atlas.strips) == 1, len(atlas.strips)
+		
+		# this one should pack into the same strip
+		map = self.atlasManager.add( zeros( (64,64,4),'B' ) )
+		assert len(self.atlasManager.components) == 1
+		assert len(self.atlasManager.components[4]) == 1
+		atlas = self.atlasManager.components[4][0]
+		assert map.atlas is atlas
+		assert len(atlas.strips) == 1, len(atlas.strips)
+		
+		# this one should pack into another strip
+		map = self.atlasManager.add( zeros( (32,32,4),'B' ) )
+		assert len(self.atlasManager.components) == 1
+		assert len(self.atlasManager.components[4]) == 1
+		atlas = self.atlasManager.components[4][0]
+		assert map.atlas is atlas
+		assert len(atlas.strips) == 2, len(atlas.strips)
+
+	def test_matrix( self ):
+		"""Test that a texture matrix can produce a proper scale/offset"""
+		map = self.atlasManager.add( zeros( (64,64,4),'B' ) )
+		matrix = map.matrix()
+		assert matrix is not None 
+		bottom_left = dot( array( [0,0,0],'f'), matrix )
+		assert allclose( bottom_left, [0,0,0] ), bottom_left
+		top_right = dot( array( [1,1,0],'f'), matrix )
+		assert allclose( top_right, [.25,.25,0] ), top_right
