c9354f8c990ae2bb637405bd95ded82a281319e1
Mike C. Fletcher <mcfletch@sturm>
2009-04-26 18:18:08 -0400

    Add texture coordinate support

--- tests/shader_sphere.py
+++ tests/shader_sphere.py
@@ -9,6 +9,7 @@ from OpenGL.GL import *
 from OpenGL.arrays import vbo
 from OpenGLContext.arrays import *
 from OpenGLContext.scenegraph.shaders import *
+from OpenGLContext.scenegraph.basenodes import ImageTexture
 
 def sphere( phi=pi/8.0 ):
 	"""Create arrays for rendering a unit-sphere
@@ -22,10 +23,12 @@ def sphere( phi=pi/8.0 ):
 	ystep = len(steps)
 	zstep = len(zsteps)
 	xstep = 1
-	coords = zeros((zstep,ystep,3), 'f')
+	coords = zeros((zstep,ystep,5), 'f')
 	coords[:,:,0] = sin(steps)
 	coords[:,:,1] = cos(zsteps).reshape( (-1,1))
 	coords[:,:,2] = cos(steps)
+	coords[:,:,3] = steps/(2*pi)
+	coords[:,:,4] = zsteps.reshape( (-1,1))/ pi
 	
 	# now scale by sin of y's 
 	scale = sin(zsteps).reshape( (-1,1))
@@ -40,7 +43,8 @@ def sphere( phi=pi/8.0 ):
 	yoffsets = arange(0,zstep-1,1,dtype='H').reshape( (-1,1,1))
 	#yoffsets = (xoffsets * ystep).reshape( (-1,1,1,))
 	indices += (yoffsets * ystep)
-	return coords.reshape((-1,3)), indices.reshape((-1,))
+	print 'tex coordinates', coords.reshape((-1,5))[:,3:5]
+	return coords.reshape((-1,5)), indices.reshape((-1,))
 	
 
 
@@ -60,22 +64,27 @@ class TestContext( BaseContext ):
 	"""
 	
 	def OnInit( self ):
-		coords,indices = sphere( pi/64 )
+		coords,indices = sphere( pi/8 )
 		print 'coordinates', coords.shape
 #		print 'indices', indices
 #		print 'taken', take(coords,indices).reshape( (-1,3))
 		self.coordLength = len(indices)
 		self.coords = vbo.VBO( coords )
 		self.indices = vbo.VBO( indices, target = 'GL_ELEMENT_ARRAY_BUFFER' )
-		glEnableClientState(GL_VERTEX_ARRAY);
-		glEnableClientState(GL_NORMAL_ARRAY);
+		glEnableClientState(GL_VERTEX_ARRAY)
+		glEnableClientState(GL_NORMAL_ARRAY)
+		glEnableClientState(GL_TEXTURE_COORD_ARRAY)
+		self.texture = ImageTexture( url = ["nehe_glass.bmp"] )
+		
 	
 	def Render( self, mode = 0):
 		"""Render the geometry for the scene."""
 		BaseContext.Render( self, mode )
+		self.texture.render( mode=mode )
 		self.coords.bind()
-		glVertexPointer( 3, GL_FLOAT,0,self.coords)
-		glNormalPointer( GL_FLOAT,0,self.coords )
+		glVertexPointer( 3, GL_FLOAT,20,self.coords)
+		glTexCoordPointer( 3, GL_FLOAT,20,self.coords+12)
+		glNormalPointer( GL_FLOAT,20,self.coords )
 		self.indices.bind()
 		glDrawElements( GL_TRIANGLES, self.coordLength, GL_UNSIGNED_SHORT, self.indices )
 		
