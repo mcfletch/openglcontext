2206033ee4f5fc2a84aea7140d7925e532af61ad
Mike C. Fletcher <mcfletch@sturm>
2009-05-06 17:08:49 -0400

    More refactoring...

--- OpenGLContext/scenegraph/quadrics.py
+++ OpenGLContext/scenegraph/quadrics.py
@@ -34,53 +34,6 @@ class Quadric( nodetypes.Geometry, node.Node ):
 			mode = None, # the renderpass object for which we compile
 		):
 		"""Render the geometry"""
-		dl = mode.cache.getData(self)
-		if not dl:
-			dl = self.compile(mode=mode)
-		if dl is None:
-			return 1
-		# okay, is now a (cached) display list object
-		dl()
-	def compile( self, mode=None ):
-		"""Compile a display-list for this geometry"""
-		dl = displaylist.DisplayList()
-		dl.start()
-		try:
-			quadratic = gluNewQuadric()
-			try:
-				self.parameters( quadratic )
-				self.generate( quadratic )
-				holder = mode.cache.holder(self, dl)
-				for field in protofunctions.getFields( self ):
-					# change to any field requires a recompile
-					holder.depend( self, field )
-				return dl
-			finally:
-				gluDeleteQuadric( quadratic )
-		finally:
-			dl.end()
-	def parameters( self, quadratic ):
-		"""Setup quadratic rendering parameters"""
-		gluQuadricNormals(quadratic, GLU_SMOOTH)
-		gluQuadricTexture(quadratic, GL_TRUE)
-	def generate( self, quadratic ):
-		"""Do the actual per-node generation of the geometry"""
-		
-	slices = field.newField( ' slices', 'SFInt32', 1, 12 )
-	stacks = field.newField( ' stacks', 'SFInt32', 1, 12 )
-
-class Sphere( basenodes.Sphere ):
-	"""Sphere geometry rendered with GLU quadratic calls"""
-	_unitSphere = None
-	def render (
-			self,
-			visible = 1, # can skip normals and textures if not
-			lit = 1, # can skip normals if not
-			textured = 1, # can skip textureCoordinates if not
-			transparent = 0,
-			mode = None, # the renderpass object for which we compile
-		):
-		"""Render the geometry"""
 		vbos = mode.cache.getData(self)
 		if not vbos:
 			vbos = self.compile( mode = mode )
@@ -113,7 +66,14 @@ class Sphere( basenodes.Sphere ):
 			glPopClientAttrib()
 			coords.unbind()
 			indices.unbind()
-			
+	def compile( self, mode=None ):
+		"""Compile this sphere for use on mode"""
+		raise NotImplementedError( """Haven't implemented %s compilation yet"""%(self.__class__.__name__,))
+		
+
+class Sphere( basenodes.Sphere, Quadric ):
+	"""Sphere geometry rendered with GLU quadratic calls"""
+	_unitSphere = None
 	def compile( self, mode=None ):
 		"""Compile this sphere for use on mode"""
 		if self._unitSphere is None:
@@ -196,51 +156,8 @@ class Sphere( basenodes.Sphere ):
 			( (self, 'radius'), ),
 		)
 		
-class Cone( basenodes.Cone ):
+class Cone( basenodes.Cone, Quadric ):
 	"""Cone geometry rendered with GLU quadratic calls"""
-	stacks = field.newField( ' stacks', 'SFInt32', 1, 2 )
-	loops = field.newField( ' loops', 'SFInt32', 1, 1 )
-	def render (
-			self,
-			visible = 1, # can skip normals and textures if not
-			lit = 1, # can skip normals if not
-			textured = 1, # can skip textureCoordinates if not
-			transparent = 0,
-			mode = None, # the renderpass object for which we compile
-		):
-		"""Render the geometry"""
-		vbos = mode.cache.getData(self)
-		if not vbos:
-			vbos = self.compile( mode = mode )
-		if vbos is None:
-			return 1
-		coords,indices,count = vbos
-		glPushClientAttrib(GL_CLIENT_ALL_ATTRIB_BITS)
-		glPushAttrib(GL_ALL_ATTRIB_BITS)
-		try:
-			coords.bind()
-			glEnableClientState(GL_VERTEX_ARRAY)
-			glVertexPointer( 3, GL_FLOAT,32,coords)
-			if visible:
-				if textured:
-					glEnableClientState(GL_TEXTURE_COORD_ARRAY)
-					glTexCoordPointer( 3, GL_FLOAT,32,coords+12)
-				if lit:
-					glEnableClientState(GL_NORMAL_ARRAY)
-					glNormalPointer( GL_FLOAT,32,coords+20 )
-			# TODO: sort for transparent geometry...
-			indices.bind()
-			# Can loop loading matrix and calling just this function 
-			# for each sphere you want to render...
-			# include both scale and position in the matrix...
-			glDrawElements( 
-				GL_TRIANGLES, count, GL_UNSIGNED_SHORT, indices
-			)
-		finally:
-			glPopAttrib()
-			glPopClientAttrib()
-			coords.unbind()
-			indices.unbind()
 	def compile( self, mode=None ):
 		"""Compile this sphere for use on mode"""
 		coords,indices = self.cone( self.height, self.bottomRadius, self.bottom, self.side )
@@ -252,22 +169,24 @@ class Cone( basenodes.Cone ):
 	
 	@classmethod
 	def cone( 
-		cls, height=2.0, bottomRadius=1.0, bottom=True, side=True,
-		phi = pi/16, longAngle=(pi*2)
+		cls, height=2.0, radius=1.0, bottom=True, side=True,
+		phi = pi/16, longAngle=(pi*2), top=False
 	):
 		"""Generate a VBO data-set to render a cone"""
 		tip = (0,height/2.0,0)
 		longsteps = arange( 0,longAngle+0.000003, phi )
 		ystep = len(longsteps)
 		zstep = 0
+		if top:
+			zstep += 2
 		if side:
 			zstep += 2
 		if bottom:
 			zstep += 2
 		# need top-ring coords and 2 sets for 
 		coords = zeros( (zstep,ystep,8), 'f')
-		coords[:,:,0] = sin(longsteps) * bottomRadius
-		coords[:,:,2] = cos(longsteps) * bottomRadius
+		coords[:,:,0] = sin(longsteps) * radius
+		coords[:,:,2] = cos(longsteps) * radius
 		coords[:,:,3] = longsteps/(2*pi)
 		def fill_disk( area ):
 			"""fill in disk elements for given area"""
@@ -281,19 +200,35 @@ class Cone( basenodes.Cone ):
 			area[1,:,3:5] = .5
 			# normal for the disk is all the same...
 			area[:,:,5:8] = (0,-1,0)
-		if side:
-			coords[0,:,0:3] = (0,height/2.0,0)
-			coords[1:4,:,1] = -height/2.0
-			coords[0,:,4] = 0
-			coords[1,:,4] = 1.0
+		def fill_sides( area ):
+			"""Fill in side-of-cylinder/cone components"""
+			area[0,:,0:3] = (0,height/2.0,0)
+			area[1:4,:,1] = -height/2.0
+			area[0,:,4] = 0
+			area[1,:,4] = 1.0
 			# normals for the sides...
-			coords[0:2,:-1,5:8] = vectorutilities.normalise(
+			area[0:2,:-1,5:8] = vectorutilities.normalise(
 				vectorutilities.crossProduct( 
-					coords[0,:-1,0:3] - coords[1,:-1,0:3],
-					coords[1,:-1,0:3] - coords[1,1:,0:3]
+					area[0,:-1,0:3] - area[1,:-1,0:3],
+					area[1,:-1,0:3] - area[1,1:,0:3]
 				)
 			)
-			coords[0:2,-1,5:8] = coords[0:2,0,5:8]
+			area[0:2,-1,5:8] = area[0:2,0,5:8]
+			
+		if side:
+			fill_sides( coords[0:2] )
+#			coords[0,:,0:3] = (0,height/2.0,0)
+#			coords[1:4,:,1] = -height/2.0
+#			coords[0,:,4] = 0
+#			coords[1,:,4] = 1.0
+#			# normals for the sides...
+#			coords[0:2,:-1,5:8] = vectorutilities.normalise(
+#				vectorutilities.crossProduct( 
+#					coords[0,:-1,0:3] - coords[1,:-1,0:3],
+#					coords[1,:-1,0:3] - coords[1,1:,0:3]
+#				)
+#			)
+#			coords[0:2,-1,5:8] = coords[0:2,0,5:8]
 			if bottom:
 				# disk texture coordinates
 				fill_disk( coords[2:4] )
@@ -321,35 +256,6 @@ class Cone( basenodes.Cone ):
 		indices = concatenate( new_indices )
 		return coords.reshape( (-1,8)), indices.reshape( (-1,))
 	
-	def generate( self, quadratic ):
-		"""Do the actual per-node generation of the geometry"""
-		# missing bottom and boolean for bottom
-		glPushMatrix()
-		try:
-			glTranslated( 0,-self.height/2.0, 0)
-			glRotated( -90, 1,0,0)
-			if self.side:
-				gluCylinder(
-					quadratic,
-					self.bottomRadius,
-					0.0,
-					self.height,
-					self.slices,
-					self.stacks,
-				)
-			if self.bottom:
-				# draw a disk facing downward from the current position
-				glRotated( 180, 1,0,0)
-				gluDisk(
-					quadratic,
-					0.0,
-					self.bottomRadius,
-					self.slices,
-					self.loops,
-				)
-		finally:
-			glPopMatrix()
-
 	def boundingVolume( self, mode=None ):
 		"""Create a bounding-volume object for this node
 
