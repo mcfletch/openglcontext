1267afc729816b1f2dff2e7c023accaca9ce2383
Mike C. Fletcher <mcfletch@sturm>
2009-07-20 01:08:33 -0400

    Make the sample more obvious and less automated.

--- tests/shader_3.py
+++ tests/shader_3.py
@@ -1,5 +1,24 @@
 #! /usr/bin/env python
-'''Shader sample-code for OpenGLContext
+'''=Shader-based Rendering: Uniform Values (Fog)=
+
+[shader_3.py-screen-0001.png Screenshot]
+
+This tutorial builds on the previous tutorial by:
+
+	* defining uniform values in shaders 
+	* passing values to uniform values from Python
+	* doing some basic calculations during the vertex 
+		shader, including defining local variables
+		and using some simple functions
+	* creating a "depth cue" via a simple "fog" function 
+		which alters the colour of each vertex according
+		the the vertex' distance from the eye.
+
+Note: the shader in this example comes (loosely) from the 
+[http://www.3dshaders.com/ OpenGL Shading Language (Orange Book)]
+Chapter 9.
+
+Our imports are by now quite familiar...
 '''
 from OpenGLContext import testingcontext
 BaseContext, MainFunction = testingcontext.getInteractive()
@@ -7,7 +26,6 @@ from OpenGL.GL import *
 from OpenGL.arrays import vbo
 from OpenGL.GL.shaders import *
 from OpenGLContext.arrays import *
-from OpenGL.GLUT import glutSolidTeapot
 
 class TestContext( BaseContext ):
 	"""This shader adds a simple linear fog to the shader
@@ -15,31 +33,28 @@ class TestContext( BaseContext ):
 	Shows use of uniforms, and a few simple calculations 
 	within the vertex shader...
 	"""
-	UNIFORM_VALUES = (
-		(glUniform1f,'end_fog',(12,)),
-		(glUniform4f,'fog_color',(0,0,0,1)),
-	)
 	def OnInit( self ):
 		self.shader = compileProgram(
-			compileShader('''
+			compileShader("""
 			uniform float end_fog;
 			uniform vec4 fog_color;
 			void main() {
 				float fog; // amount of fog to apply
 				float fog_coord; // distance for fog calculation...
+				// This function is generally faster and is guaranteed
+				// to produce the same result on each run...
+				// gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
 				gl_Position = ftransform();
-				//gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
-				// ecPosition is the eye-coordinate position...
 				
 				fog_coord = abs(gl_Position.z);
 				fog_coord = clamp( fog_coord, 0.0, end_fog);
 				fog = (end_fog - fog_coord)/end_fog;
 				fog = clamp( fog, 0.0, 1.0);
 				gl_FrontColor = mix(fog_color, gl_Color, fog);
-			}''',GL_VERTEX_SHADER),
-			compileShader('''void main() {
+			}""",GL_VERTEX_SHADER),
+			compileShader("""void main() {
 				gl_FragColor = gl_Color;
-			}''',GL_FRAGMENT_SHADER),
+			}""",GL_FRAGMENT_SHADER),
 		)
 		self.vbo = vbo.VBO(
 			array( [
@@ -55,18 +70,16 @@ class TestContext( BaseContext ):
 				[  2, 1, 0,  0,1,1 ],
 			],'f')
 		)
-		self.UNIFORM_LOCATIONS = {}
-		for func,var,values in self.UNIFORM_VALUES:
-			loc = glGetUniformLocation( self.shader, var )
-			self.UNIFORM_LOCATIONS[var] = loc
-	
+		self.UNIFORM_LOCATIONS = {
+			'end_fog': glGetUniformLocation( self.shader, 'end_fog' ),
+			'fog_color': glGetUniformLocation( self.shader, 'fog_color' ),
+		}
 	def Render( self, mode = 0):
 		"""Render the geometry for the scene."""
 		BaseContext.Render( self, mode )
 		glUseProgram(self.shader)
-		for func,var,values in self.UNIFORM_VALUES:
-			loc = self.UNIFORM_LOCATIONS[var]
-			func( loc, *values )
+		glUniform1f( self.UNIFORM_LOCATIONS['end_fog'],12)
+		glUniform4f(self.UNIFORM_LOCATIONS['fog_color'],1,1,1,1)
 		glRotate( 45, 0,1,0 )
 		glScale( 3,3,3 )
 		try:
@@ -79,6 +92,8 @@ class TestContext( BaseContext ):
 				glDrawArrays(GL_TRIANGLES, 0, 9)
 			finally:
 				self.vbo.unbind()
+				glDisableClientState(GL_VERTEX_ARRAY);
+				glDisableClientState(GL_COLOR_ARRAY);
 		finally:
 			glUseProgram( 0 )
 		
