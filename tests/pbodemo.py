41f88f89b478f5f2416b0f8c7f89800d62bf8019
Mike C. Fletcher <mcfletch@vrplumber.com>
2009-09-01 16:53:00 -0400

    Add a pixel-buffer-object test/demo

---
+++ tests/pbodemo.py
@@ -0,0 +1,83 @@
+#! /usr/bin/env python
+'''Tests/demos pixel-buffer-object operations in OpenGL
+'''
+import OpenGL 
+#OpenGL.USE_ACCELERATE = False
+OpenGL.FULL_LOGGING = True
+from OpenGLContext import testingcontext
+BaseContext, MainFunction = testingcontext.getInteractive()
+from OpenGLContext.scenegraph.basenodes import *
+from OpenGL.GL import *
+from OpenGLContext.arrays import array, frombuffer
+from OpenGL.arrays import vbo
+import string, time
+import ctypes,weakref
+
+_cleaners = {}
+
+def _cleaner( vbo ):
+    def clean( ref ):
+        try:
+            _cleaners.pop( vbo )
+        except Exception, err:
+            pass
+        else:
+            glUnmapBuffer( vbo.target )
+    return clean
+
+def map_buffer( vbo, access=GL_READ_WRITE ):
+    """Map the given buffer into a numpy array..."""
+    func = ctypes.pythonapi.PyBuffer_FromMemory
+    func.restype = ctypes.py_object
+    vp = glMapBuffer( vbo.target, access )
+    buffer = func( 
+        ctypes.c_void_p(vp), vbo.size 
+    )
+    array = frombuffer( buffer, 'B' )
+    _cleaners[vbo] = weakref.ref( array, _cleaner( vbo ))
+    return array
+
+
+class TestContext( BaseContext ):
+    currentImage = 0
+    currentSize = 0
+    def Render( self, mode = 0):
+        BaseContext.Render( self, mode )
+        self.shape.Render( mode )
+        width,height = self.getViewPort()
+        self.vbo.bind()
+        glReadPixels(
+            0, 0, 
+            300, 300, 
+            GL_RGBA, 
+            GL_UNSIGNED_BYTE,
+            self.vbo,
+        )
+        data = map_buffer( self.vbo )
+        print len([x for x in data if x != 255]), 'non-white pixels'
+        del data
+        self.vbo.unbind()
+        
+    def OnInit( self ):
+        """Scene set up and initial processing"""
+        self.vbo = vbo.VBO( 
+            None,
+            target = GL_PIXEL_PACK_BUFFER,
+            usage = GL_DYNAMIC_READ,
+            size = 300*300*4,
+        )
+        self.shape = Shape(
+            geometry = Teapot( size=2.5 ),
+            appearance = Appearance(
+                material = Material(
+                    diffuseColor =(1,1,1)
+                ),
+                texture = ImageTexture(
+                    url = ["nehe_wall.bmp"]
+                ),
+            ),
+        )
+
+if __name__ == "__main__":
+    MainFunction ( TestContext)
+
