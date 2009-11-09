6542158d0efacd0fb242d1ada70c18356c94423d
Mike C. Fletcher <mcfletch@vrplumber.com>
2009-11-09 13:18:36 -0500

    Whitespace

--- tests/simplebox.py
+++ tests/simplebox.py
@@ -4,30 +4,30 @@ BaseContext = testingcontext.getInteractive()
 from OpenGLContext.scenegraph.basenodes import *
 
 scene = sceneGraph(
-	children = [
-		Group(
-			DEF = 'g',
-		),
-	],
+    children = [
+        Group(
+            DEF = 'g',
+        ),
+    ],
 )
 scene.children[0].children.append( Transform(
-	DEF = 't',
-	translation = ( 1,2,3 ),
-	children = [
-		Shape(
-			geometry = Box(),
-			# follows VRML97 standard, so normally
-			# want the material Node so that there
-			# will be lighting applied...
-			appearance=Appearance(
-				material = Material(),
-			),
-		),
-	],
+    DEF = 't',
+    translation = ( 1,2,3 ),
+    children = [
+        Shape(
+            geometry = Box(),
+            # follows VRML97 standard, so normally
+            # want the material Node so that there
+            # will be lighting applied...
+            appearance=Appearance(
+                material = Material(),
+            ),
+        ),
+    ],
 ))
 class TestContext( BaseContext ):
-	def OnInit( self ):
-		self.sg = scene
+    def OnInit( self ):
+        self.sg = scene
 
 if __name__ == "__main__":
-	TestContext.ContextMainLoop()
+    TestContext.ContextMainLoop()
