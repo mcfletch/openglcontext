a5fb0b9599cba4ed462d4198660961eab241ecc7
Mike C. Fletcher <mcfletch@vrplumber.com>
2009-11-13 07:50:06 -0500

    Reformat and make the builddocs script executable.

--- pydoc/builddocs.py
+++ pydoc/builddocs.py
@@ -1,46 +1,38 @@
+#! /usr/bin/env python
 """Script to automatically generate OpenGLContext documentation"""
 import gldoc, pydoc2
 
 if __name__ == "__main__":
-	excludes = [
-		"OpenGL.GL",
-		"OpenGL.GLU",
-		"OpenGL.GLUT",
-		"OpenGL.GLE",
-		"OpenGL.GLX",
-		"wxPython.wx",
-		"Numeric",
-		"numpy",
-		"_tkinter",
-		"Tkinter",
-		"math",
-		"string",
-		"pygame",
-		"pygame.locals",
-	]
-	stops = [
-		"OpenGL.Demo.NeHe",
-		"OpenGL.Demo.GLE",
-		"OpenGL.Demo.da",
-	]
-
-	modules = [
-		"OpenGLContext",
-		"wxPython.glcanvas",
-		"vrml",
-		'logging',
-		"OpenGL",
-		"ttfquery",
-		"simpleparse",
-		"fontTools",
-		"numpy",
-		"Numeric",
-	]	
-	pydoc2.PackageDocumentationGenerator(
-		baseModules = modules,
-		destinationDirectory = ".",
-		exclusions = excludes,
-		recursionStops = stops,
-		formatter = gldoc.GLFormatter(),
-	).process ()
-	
+    excludes = [
+        "wxPython.wx",
+        "Numeric",
+        "numpy",
+        "_tkinter",
+        "Tkinter",
+        "math",
+        "string",
+        "pygame",
+        "pygame.locals",
+    ]
+    modules = [
+        "OpenGLContext",
+        "OpenGLContext_qt",
+        "wxPython.glcanvas",
+        "vrml",
+        "vrml_accelerate",
+        'logging',
+        "OpenGL",
+        "OpenGL_accelerate",
+        "ttfquery",
+        "simpleparse",
+        "fontTools",
+        "numpy",
+    ]	
+    pydoc2.PackageDocumentationGenerator(
+        baseModules = modules,
+        destinationDirectory = ".",
+        exclusions = excludes,
+        #recursionStops = stops,
+        formatter = gldoc.GLFormatter(),
+    ).process ()
+    
