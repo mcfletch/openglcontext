029edca18455525630e9f2b0ed57f005066c09d1
Mike C. Fletcher <mcfletch@vrplumber.com>
2009-12-12 13:36:55 -0500

    Add more tests, tweak ordering

--- tests/runalltests.py
+++ tests/runalltests.py
@@ -9,7 +9,7 @@ SUCCESS_RETURN_CODE = 0
 def runTest( name ):
     """Execute a single test using gltest.py"""
     full_name = os.path.abspath( name )
-    returnCode = subprocess.call( 
+    returnCode = subprocess.call(
         [ PYTHON, TEST_RUNNER, full_name ],
         shell = False,
         cwd = os.path.dirname( full_name ),
@@ -17,81 +17,79 @@ def runTest( name ):
     return returnCode
 
 scripts = """addnodes.py
-arbocclusionquery.py 
+arbocclusionquery.py
 arbpointparameters.py
-arbwindowpos.py      
+arbwindowpos.py
 ativertexarrayobject.py
-backgroundobject.py    
-boxobject.py           
-boxtextured.py         
-coneobject.py          
-cubeback_rot.py        
+backgroundobject.py
+boxobject.py
+boxtextured.py
+coneobject.py
+cubeback_rot.py
 cubebackgroundobject.py
-cylinderobject.py      
-dek_surf.py            
-dek_texturesurf.py     
-feedback_mode.py       
-gearobject.py          
-getteximage.py         
-glarrayelement.py      
-gldrawarrays.py        
-gldrawarrays_string.py 
-gldrawelements.py      
-gldrawelements_list.py 
+cylinderobject.py
+dek_surf.py
+dek_texturesurf.py
+feedback_mode.py
+gearobject.py
+getteximage.py
+glarrayelement.py
+gldrawarrays.py
+gldrawarrays_string.py
+gldrawelements.py
+gldrawelements_list.py
 gldrawelements_string.py
-gldrawpixels.py         
-gldrawpixelssynth.py    
-glelathe.py             
-glget_with_fonts.py     
-glhistogram.py          
-glinterleavedarrays.py  
-glu_tess.py             
-glu_tess2.py            
-glut_bitmap_font.py     
-glut_font.py            
-glut_fullscreen.py      
-glutbitmapcharacter.py  
-glutmousewheel.py       
-glvertex.py             
-heightmap.py            
+gldrawpixels.py
+gldrawpixelssynth.py
+glelathe.py
+glget_with_fonts.py
+glhistogram.py
+glinterleavedarrays.py
+glu_tess.py
+glu_tess2.py
+glut_bitmap_font.py
+glut_font.py
+glut_fullscreen.py
+glutbitmapcharacter.py
+glutmousewheel.py
+glvertex.py
+heightmap.py
 indexedfaceset_lit_npf.py
 indexedfaceset_lit_npv.py
-indexedfacesetobject.py  
-indexedlinesetobject.py  
-lightobject.py           
-materialobject.py        
-molehill.py              
-mouseover.py             
-multitexture_1.py        
-nehe1.py                 
-nehe2.py                 
-nehe3.py                 
-nehe4.py                 
-nehe5.py                 
-nehe6.py                 
-nehe6_convolve.py        
-nehe6_multi.py           
-nehe6_timer.py           
-nehe7.py                 
-nehe8.py                 
-node_modify.py           
-nurbsobject.py           
-particles_simple.py      
-pbodemo.py               
-point_and_click.py       
-pointsetobject.py        
-polygonal_text.py        
-redbook_alpha.py         
-redbook_alpha3D.py       
-redbook_surface.py       
-redbook_surface_cb.py    
-redbook_trim.py          
-selectrendermode.py      
+indexedfacesetobject.py
+indexedlinesetobject.py
+lightobject.py
+materialobject.py
+molehill.py
+mouseover.py
+multitexture_1.py
+nehe1.py
+nehe2.py
+nehe3.py
+nehe4.py
+nehe5.py
+nehe6.py
+nehe6_convolve.py
+nehe6_multi.py
+nehe6_timer.py
+nehe7.py
+nehe8.py
+node_modify.py
+nurbsobject.py
+particles_simple.py
+pbodemo.py
+point_and_click.py
+pointsetobject.py
+polygonal_text.py
+redbook_alpha.py
+redbook_alpha3D.py
+redbook_surface.py
+redbook_surface_cb.py
+redbook_trim.py
+selectrendermode.py
 selectrendermode_threads.py
-shader_1.py                
-shader_10.py               
-shader_11.py               
-shader_2.py                
+shader_1.py
+shader_2.py
 shader_2_c_void_p.py
 shader_3.py
 shader_4.py
@@ -100,11 +98,16 @@ shader_6.py
 shader_7.py
 shader_8.py
 shader_9.py
+shader_10.py
+shader_11.py
+shader_12.py
 shader_sphere.py
 shader_spike.py
 shadergeometry.py
 shaderobjects.py
 shaders.py
+shadow_1.py
+shadow_2.py
 simplebox.py
 simplerotate.py
 solid_font.py
@@ -182,7 +185,7 @@ def main(scripts):
         sys.stderr.write( 'Success' )
         sys.stderr.write( '\n' )
     webbrowser.open_new_tab( os.path.join('test-results','index.html' ))
-    
+
 
 if __name__ == "__main__":
     if sys.argv[1:]:
