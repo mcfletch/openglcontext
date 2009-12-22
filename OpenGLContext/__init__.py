d4e01fcaf17f8baa5e372d0afcbb7a91db1c0cc4
Mike C. Fletcher <mcfletch@vrplumber.com>
2009-12-21 19:37:25 -0500

    Bump version for alpha 11

--- OpenGLContext/__init__.py
+++ OpenGLContext/__init__.py
@@ -19,7 +19,7 @@ modules should operate under any of the fully functional
 GUI contexts (note that there are unfinished contexts).
 '''
 
-__version__ = "2.1.0a10"
+__version__ = "2.1.0a11"
 __author__ = "Michael Colin Fletcher"
 __license__ = "BSD-Style, see license.txt for details and exceptions"
 
