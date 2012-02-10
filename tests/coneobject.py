1313dc8f1c1b410d7936448cbcb4ea1284460530
Mike C. Fletcher <mcfletch@vrplumber.com>
2012-02-10 12:34:04 -0500

    Make the cone-object test a more generic test...

--- tests/coneobject.py
+++ tests/coneobject.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 '''Tests rendering of the Box geometry object
 '''
+import OpenGL
+#OpenGL.FULL_LOGGING = True
 from OpenGLContext import testingcontext
 BaseContext = testingcontext.getInteractive()
 from OpenGLContext.scenegraph.basenodes import *
@@ -12,15 +14,16 @@ images = [
     "nehe_glass.bmp",
     "pimbackground_FR.jpg",
     "nehe_wall.bmp",
+    "marbleface.jpeg",
 ]
 
-sizes = [
-    .5,
+cone_sizes = [
     1,
     2,
     3,
     4,
-    10,
+    .5,
+    .25,
 ]
 
 class TestContext( BaseContext ):
@@ -28,8 +31,8 @@ class TestContext( BaseContext ):
     currentSize = 0
     def OnInit( self ):
         """Scene set up and initial processing"""
-        print """You should see an elongated box over a white background
-    The box should have a mapped texture (a stained-glass window)
+        print """You should see a cone over a black background
+    The cone should have a mapped texture (a stained-glass window)
     and should be centered on the window.
 """
         print 'press i to choose another texture for the box'
@@ -40,34 +43,78 @@ class TestContext( BaseContext ):
         self.addEventHandler(
             'keypress', name = 's', function = self.OnSizeSwitch
         )
-        self.sg = self.shape = Shape(
-            geometry = Cone( ),
-            appearance = Appearance(
-                material = Material(
-                    diffuseColor =(1,1,1)
-                ),
-                texture = ImageTexture(
-                    url = [images[0]]
-                ),
+        self.appearance = Appearance(
+            material = Material(
+                diffuseColor =(1,0,0),
+                specularColor = (1,1,1),
             ),
+            texture = ImageTexture(
+                url = [images[0]]
+            ),
+        )
+        self.cone = Shape(
+            geometry = Cone( ),
+            appearance = self.appearance,
+        )
+        self.cylinder = Shape(
+            geometry = Cylinder( ),
+            appearance = self.appearance,
+        )
+        self.box = Shape(
+            geometry = Box(),
+            appearance = self.appearance,
+        )
+        self.gear = Shape(
+            geometry = Gear(),
+            appearance = self.appearance,
+        )
+        self.sg = Transform(
+            children = [
+                Transform(
+                    translation = (4,0,0),
+                    children = [self.cone],
+                ),
+                Transform(
+                    translation = (0,0,0),
+                    children = [self.box],
+                ),
+                Transform(
+                    translation = (-4,0,0),
+                    children = [self.cylinder],
+                ),
+                Transform(
+                    translation = (0,4,0),
+                    children = [self.gear],
+                    scale = (3,3,3),
+                ),
+                SimpleBackground(
+                    color = (1,1,1),
+                ),
+            ],
+            scale = (.5,.5,.5),
         )
     def OnImageSwitch( self, event=None ):
         """Choose a new mapped texture"""
         self.currentImage = currentImage = self.currentImage+1
         newImage = images[currentImage%len(images)]
-        self.shape.appearance.texture.url = [ newImage ]
+        self.appearance.texture.url = [ newImage ]
         print "new image (loading) ->", newImage
     def OnSizeSwitch( self, event=None ):
         """Choose a new size"""
         self.currentSize = currentSize = self.currentSize+1
-        newSize = sizes[currentSize%len(sizes)]
-        self.shape.geometry.bottomRadius = newSize
-        newSize = sizes[(currentSize+1)%len(sizes)]
-        self.shape.geometry.height = newSize
+        newSize = cone_sizes[currentSize%len(cone_sizes)]
+        self.cone.geometry.bottomRadius = newSize
+        newSize = cone_sizes[(currentSize+1)%len(cone_sizes)]
+        self.cone.geometry.height = newSize
+        
+        self.box.geometry.size = (newSize,newSize,newSize)
+        self.cylinder.geometry.height = newSize
+        self.cylinder.geometry.radius = newSize * .25
+        self.gear.geometry.outer_radius = newSize * .25
         
         print "new size ->", newSize
-        self.triggerRedraw(1)
-    
+        self.triggerRedraw(True)
+        
 
 if __name__ == "__main__":
     TestContext.ContextMainLoop()
