b35a9148e50cf0cefd4edccf04e338ff2d637916
Mike C. Fletcher <mcfletch@vrplumber.com>
2012-02-10 10:18:50 -0500

    Make the output of bounding volume test less scary; now just tells you when accurracy starts to degrade...

--- tests/boundingvolume.py
+++ tests/boundingvolume.py
@@ -2,7 +2,8 @@ from OpenGLContext.scenegraph import boundingvolume
 from OpenGLContext import frustum, utilities
 from OpenGLContext.arrays import *
 from OpenGL.GL import *
-import traceback
+import traceback, logging 
+log = logging.getLogger( 'test.boundingvolume' )
 
 def testPN2P():
     """Tests that utilities functions operate as expected"""
@@ -36,21 +37,22 @@ def testExclusion():
         center = (-3,0,0),
         size = (1,1,1),
     )
-    assert v.visible( f, identity(4,'d'))
+    assert v.visible( f, identity(4,'f'))
     v = boundingvolume.AABoundingBox(
         center = (3,0,0),
         size = (1,1,1),
     )
-    assert not v.visible( f, identity(4,'d'))
+    assert not v.visible( f, identity(4,'f'))
     # test right at the border of the box...
     v = boundingvolume.AABoundingBox(
         center = (.5,0,0),
         size = (1,1,1),
     )
-    assert v.visible( f, identity(4,'d'))
+    assert v.visible( f, identity(4,'f'))
 
 
 if __name__ == '__main__':
+    logging.basicConfig( level = logging.WARNING )
     testPN2P()
     testExclusion()
     # now test the stuff that needs a run-time context...
@@ -59,35 +61,37 @@ if __name__ == '__main__':
     class TestContext( BaseContext ):
         currentImage = 0
         currentSize = 0
-        def Viewpoint( self, mode = None ):
+        def Render( self, mode = None ):
+            glMatrixMode(GL_MODELVIEW)
+            glLoadIdentity()
             glMatrixMode(GL_PROJECTION);
             # load the identity matrix (reset the view)
             # calculate a 3D perspective view
             print '___________________________'
             from OpenGL.GL import glFrustum
             for near in (.2,1.,3.,4.,5.,19.):
-                print 'near', near
                 for far in arange(20.0,2**31,1000):
                     try:
                         glLoadIdentity()
                         glFrustum( -20,20, -20,20, near, far )
                         f = frustum.Frustum.fromViewingMatrix()
                         farCurrent,nearCurrent = f.planes[-2:]
-##						nearCurrent,farCurrent = round(nearCurrent,5),round(farCurrent,5)
                         farCurrent = round(farCurrent[-1],4)
                         nearCurrent = round(nearCurrent[-1],4)
                         deltaFar = abs(farCurrent-far)
                         deltaNear = abs( nearCurrent+near)
-                        assert deltaFar < abs(far/100), "Far was %s, should have been ~ %s, delta was %s"%(farCurrent,far, deltaFar)
+                        
+                        assert deltaFar < abs(far/100), "Far was %s, should have been ~ %s, delta was %s (%.3f%%)"%(farCurrent,far, deltaFar, 100*deltaFar/far)
                         assert deltaNear < abs(near/100), "Near was %s, should have been ~ %s, delta was %s"%(nearCurrent,near,deltaNear)
-                    except:
-                        traceback.print_exc()
-                        print '  max far reached', far
-                        try:
-                            print '  frustum planes', f.planes
-                        except:
-                            pass
+                    except AssertionError, err:
+                        log.warn(
+                            "Accuraccy < than 1%% of far with (near,far) = (%s,%s)",
+                            near,far,
+                        )
                         break
+                    except Exception, err:
+                        traceback.print_exc()
+                        self.OnQuit( )
             glMatrixMode(GL_MODELVIEW);
     TestContext.ContextMainLoop()
     
