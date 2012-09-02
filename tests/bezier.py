086dd1ee764c506c4308d4ac68cd2f4b81b4f34e
Mike C. Fletcher <mcfletch@vrplumber.com>
2012-09-01 23:48:57 -0400

    More obvious implementation via refactoring

--- tests/bezier.py
+++ tests/bezier.py
@@ -21,21 +21,38 @@ def evaluate( cps, divisions = 10 ):
     step = 1.0/(divisions-1)
     for s in arange( 0.0, 1.0 + (.5 * step), step ):
         yield epoint( s, cps )
+def control_curves( points, divisions = 10 ):
+    """Calculate control curves from control points"""
+    return array( [
+        list(evaluate(plane)) 
+        for plane in points
+    ], dtype='f' )
+    
+def patch( points, divisions = 10 ):
+    """Convert the control points into a bezier patch with given sub-divisions"""
+    # Convert the control points into control curves
+    # For each step along the control curves, calculate the resulting curve
+    return array( [
+        list(evaluate(plane, divisions)) 
+        for plane in transpose(
+            control_curves(points,divisions),
+            (1,0,2)
+        )
+    ], 'f')
 
 class TestContext( BaseContext ):
     
     def OnInit( self ):
         """Create the scenegraph for rendering"""
         points = array([
-            [(0,0,0), (1,1,0), (2,0,0)],
+            [(0,0,0), (1,0,0), (2,0,0)],
             [(0,0,1), (1,2,1), (2,0,1)],
-            [(0,0,2), (1,1,2), (2,0,2)],
+            [(0,0,2), (1,0,2), (2,0,2)],
         ], 'f')
-        points2 = array( [list(evaluate(plane)) for plane in points], dtype='f' )
-        points3 = array( [list(evaluate(plane)) for plane in transpose(points2,(1,0,2))], 'f')
+        points3 = patch( points )
         self.sg = sceneGraph( children = [
             Shape( geometry = PointSet(
-                coord = Coordinate( point = points2 ),
+                coord = Coordinate( point = points ),
             )),
             Shape( 
                 geometry = PointSet(
