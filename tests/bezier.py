e08a7ed1128cdf82a9c399d033812930293532a6
Mike C. Fletcher <mcfletch@vrplumber.com>
2012-10-15 17:50:06 -0400

    Start working on render-able version

--- tests/bezier.py
+++ tests/bezier.py
@@ -21,8 +21,12 @@ def weight_array( divisions ):
         for a in arange( 0.0, 1.0 + (.5 * step ), step, dtype='f' )
     ], dtype='f' )
 
-def expand( points, divisions= 10 ):
+def expand( points, divisions= 10, normals=False, texCoords=False, globalTexCoord=True ):
     """Expand points into the final data-set for positions...
+    
+    points -- array of 2N+1 x 2M+1 x 3 control points 
+    divisions -- number of divisions for each 3x3 control-point area
+    normals -- if True, auto-generate normals for the array...
     
     3N * 3M array of control points 
         CP <divisions> CP 
@@ -35,8 +39,11 @@ def expand( points, divisions= 10 ):
         return ((M - 1)//2 * divisions-1) + 1
     M,N,D = points.shape
     assert D in (3,4), """Need a 3 or 4 element point-set"""
+    if normals:
+        D += 3
+        
     expanded = zeros( (final_count(M),final_count(N),D), dtype=points.dtype )
-    expanded[::divisions-1,::divisions-1] = points[::2,::2] 
+    #expanded[::divisions-1,::divisions-1] = points[::2,::2] 
     
     # expand control curves in one direction...
     # creates a new points array to be used for other direction...
@@ -50,9 +57,18 @@ def expand( points, divisions= 10 ):
             curves = dot( ws, points[m:m+3,n:n+3] )
             # now get final points...
             verts = dot( ws, curves )
-            expanded[ m_final:m_final+divisions, n_final:n_final+divisions,:] = verts
+            expanded[ m_final:m_final+divisions, n_final:n_final+divisions,:3] = verts
+    
     return expanded
 
+def grid_indices( expanded ):
+    """Create indices array to render expanded vertex array"""
+    M,N = expanded.shape[:2]
+    quads = (M-1) * (N-1)
+    quadbases = arange( 0, quads, dtype='i')
+    quadbases = quadbases.repeat( 6 )
+    offsets = tile( array( [0,1,N,N,1,1+N], 'i' ), quads )
+    return offsets + quadbases
 
 class TestContext( BaseContext ):
     
@@ -64,16 +80,18 @@ class TestContext( BaseContext ):
             [(0,0,2), (1,0,2), (2,0,2), (3,-1,2),(4,0,2)],
         ], 'f')
         points3 = expand( points )
+        indices = grid_indices( points3 )
         
         self.sg = sceneGraph( children = [
             Shape( geometry = PointSet(
                 coord = Coordinate( point = points ),
             )),
             Shape( 
-                geometry = PointSet(
+                geometry = IndexedPolygons(
                     coord = Coordinate( point = points3 ),
+                    index = indices,
                     color = Color(
-                        color = [(1,0,0)]*len(points3)*len(points3[0]),
+                        color = [(1,0,0)]*len(indices),
                     ),
                 ),
             ),
