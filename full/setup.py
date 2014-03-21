e2aaa284b48422d8b35dd784e21175fc75e074df
Mike C. Fletcher <mcfletch@vrplumber.com>
2014-03-20 23:32:15 -0400

    Make the dependencies min-version based to pull in a modern release

--- full/setup.py
+++ full/setup.py
@@ -1,49 +1,29 @@
 #! /usr/bin/env python
 """Collector package that installs OpenGLContext with all options"""
 from setuptools import setup
-
 import sys
-if sys.hexversion < 0x2050000:
-    simpleparse = 'SimpleParse==2.0.1a3'
-    simpleparse_link = 'http://sourceforge.net/projects/simpleparse/files/simpleparse/2.0.1a3'
-else:
-    simpleparse = 'SimpleParse'
-    simpleparse_link = ''
 
 if __name__ == "__main__":
     setup (
         name = "OpenGLContext-full",
-        version = '2.2.0a1',
+        version = '3.1.0b1',
         description = "Installs all of OpenGLContext with optional dependencies",
         author = "Mike C. Fletcher",
-        author_email = "mcfletch@users.sourceforge.net",
+        author_email = "mcfletch@vrplumber.com",
         url = "http://pyopengl.sourceforge.net/context/",
         license = "BSD",
 
         install_requires = [
-            'setuptools',
-            'PyOpenGL',
-            'PyOpenGL-accelerate',
-            'OpenGLContext',
-            'PyVRML97',
-            'PyVRML97-accelerate',
-            simpleparse,
-            #'numpy',
-            # likely need to provide system-built for most users
-            # as building from source misses e.g. JPEG support on
-            # Linux...
-            #'Imaging',
+            'PyOpenGL >= 3.1.0b1',
+            'PyOpenGL-accelerate >= 3.1.0b1',
+            'OpenGLContext >= 2.3.0b1',
+            'PyVRML97 >= 2.3.0b1',
+            'PyVRML97-accelerate >= 2.3.0b1',
+            'SimpleParse',
+            'numpy',
             'PyDispatcher',
-            'fonttools',
+            'FontTools',
             'TTFQuery',
+            'pillow',
         ],
-        dependency_links = [
-            # fonttools doesn't work off SF...
-            # 'http://downloads.sourceforge.net/project/fonttools/Source%20code/2.3/fonttools-2.3.tar.gz?use_mirror=cdnetworks-us-2',
-            'http://cdnetworks-us-1.dl.sourceforge.net/project/fonttools/2.3/fonttools-2.3.tar.gz',
-            # Imaging is registered as PIL, but named Imaging...
-            'http://effbot.org/downloads/',
-            simpleparse_link,
-        ],
-
     )
