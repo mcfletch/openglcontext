79e84ee9112f8255dde64b9a25ddeade02be75ce
Mike C. Fletcher <mcfletch@vrplumber.com>
2015-11-15 23:35:34 -0500

    Unused imports

--- OpenGLContext/testingcontext.py
+++ OpenGLContext/testingcontext.py
@@ -6,12 +6,13 @@ not use this module for real world applications, as it is
 unlikely that nontrivial code will be completely stable across
 all interactive context classes."""
 
-import traceback, string, os, sys, optparse
+import optparse
 from OpenGLContext import plugins, context, contextdefinition
 
 # Test-runner can specify the base-class explicitly...
 CONFIGURED_BASE = None
 REQUIRED_EXTENSION_MISSING = 3 # process return-code for a missing extension
+
 
 def getVRML( preference= None ):
     """Retrieve the preferred testing context:
