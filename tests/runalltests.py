52c9377b4347f9aa0e0fa333d2179004c1e31e12
Mike C. Fletcher <mcfletch@vrplumber.com>
2015-11-17 14:49:05 -0500

    Scan the directory to find tests...

--- tests/runalltests.py
+++ tests/runalltests.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python
 """Script to run the "standard" set of OpenGLContext scripts (in this directory)"""
-import OpenGLContext,os,sys,subprocess,datetime, webbrowser, glob
+import os,sys,subprocess,datetime, webbrowser, glob
 from OpenGLContext import testingcontext
 PYTHON = sys.executable
-TEST_RUNNER = os.path.join( os.path.dirname( OpenGLContext.__file__ ), 'bin','gltest.py' )
+TEST_RUNNER = 'oglc-test'
 SUCCESS_RETURN_CODE = 0
 HERE = os.path.dirname(__file__)
 
@@ -11,129 +11,41 @@ def runTest( name ):
     """Execute a single test using gltest.py"""
     full_name = os.path.abspath( name )
     returnCode = subprocess.call(
-        [ PYTHON, TEST_RUNNER, full_name ],
+        [ TEST_RUNNER, full_name ],
         shell = False,
         cwd = os.path.dirname( full_name ),
     )
     return returnCode
 
 STOP_SET = set([
+    'runalltests.py',
     'frust_test_module.py',
     'numpyfields.py',
+    'wx_multiple_contexts.py',
+    'wx_with_controls.py',
+    'wx_font.py',
+    'nehe6_convolve.py', # modern drivers don't include the functionality...
+    'profile_view.py',
+    'pygame_font.py',
+    'pygame_textureatlas.py',
+    'savepostscript.py', # removed functionality
 ])
+if sys.platform != 'win32':
+    STOP_SET.add('wgl_font.py')
+    STOP_SET.add('wgl_bitmap_font.py')
+    STOP_SET.add('wglpixelformatarb.py')
+    STOP_SET.add('glprint.py')
 
 def get_scripts():
-    scripts = [
+    scripts = sorted([
         x for x in glob.glob( os.path.join( HERE, '*.py' ))
         if (
             not os.path.basename(x).startswith( '_' )
             and 
             not os.path.basename(x) in STOP_SET
         )
-    ]
+    ])
     return scripts
-
-scripts = """addnodes.py
-arbocclusionquery.py
-arbpointparameters.py
-arbwindowpos.py
-ativertexarrayobject.py
-backgroundobject.py
-boxobject.py
-boxtextured.py
-coneobject.py
-cubeback_rot.py
-cubebackgroundobject.py
-cylinderobject.py
-dek_surf.py
-dek_texturesurf.py
-feedback_mode.py
-gearobject.py
-getteximage.py
-glarrayelement.py
-gldrawarrays.py
-gldrawarrays_string.py
-gldrawelements.py
-gldrawelements_list.py
-gldrawelements_string.py
-gldrawpixels.py
-gldrawpixelssynth.py
-glelathe.py
-glget_with_fonts.py
-glhistogram.py
-glinterleavedarrays.py
-glu_tess.py
-glu_tess2.py
-glut_bitmap_font.py
-glut_font.py
-glut_fullscreen.py
-glutbitmapcharacter.py
-glutmousewheel.py
-glvertex.py
-heightmap.py
-indexedfaceset_lit_npf.py
-indexedfaceset_lit_npv.py
-indexedfacesetobject.py
-indexedlinesetobject.py
-lightobject.py
-materialobject.py
-molehill.py
-mouseover.py
-multitexture_1.py
-nehe1.py
-nehe2.py
-nehe3.py
-nehe4.py
-nehe5.py
-nehe6.py
-nehe6_convolve.py
-nehe6_multi.py
-nehe6_timer.py
-nehe7.py
-nehe8.py
-node_modify.py
-nurbsobject.py
-particles_simple.py
-pbodemo.py
-point_and_click.py
-pointsetobject.py
-polygonal_text.py
-redbook_alpha.py
-redbook_alpha3D.py
-redbook_surface.py
-redbook_surface_cb.py
-redbook_trim.py
-selectrendermode.py
-selectrendermode_threads.py
-shader_1.py
-shader_2.py
-shader_2_c_void_p.py
-shader_3.py
-shader_4.py
-shader_5.py
-shader_6.py
-shader_7.py
-shader_8.py
-shader_9.py
-shader_10.py
-shader_11.py
-shader_12.py
-shader_sphere.py
-shader_spike.py
-shadergeometry.py
-shaderobjects.py
-shaders.py
-shadow_1.py
-shadow_2.py
-simplebox.py
-simplerotate.py
-solid_font.py
-spherebackgroundobject.py
-spincube.py
-teapotobject.py
-textureobject.py
-timesensorobject.py
-transparentsorted.py""".split()
 
 examine_file = """<html><head>
     <title>Test Results</title>
@@ -208,4 +120,4 @@ if __name__ == "__main__":
     if sys.argv[1:]:
         main( sys.argv[1:] )
     else:
-        main(scripts)
+        main(get_scripts())
