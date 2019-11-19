a2ac3c71cd62ad83ddee76a3d961e427225b7829
Mike C. Fletcher <mcfletch@vrplumber.com>
2019-11-19 15:09:28 -0500

    PIL now raises a ValueError on no-packer-found

--- tests/dek_texturesurf.py
+++ tests/dek_texturesurf.py
@@ -99,7 +99,7 @@ class TestContext( BaseContext ):
         im = open(imageName)
         try:
             ix, iy, image = im.size[0], im.size[1], im.tobytes("raw", "RGBA", 0, -1)
-        except SystemError:
+        except (ValueError,SystemError):
             ix, iy, image = im.size[0], im.size[1], im.tobytes("raw", "RGBX", 0, -1)
         # generate a texture ID
         ID = glGenTextures(1)
