a864ff7fd52d3cf99925d769a91cf1bfb3aa3cf5
Mike Fletcher <vrplumber@gmail.com>
2026-09-12 01:22:42 -0400

    RELEASE 3.0.0a4

--- OpenGLContext/__init__.py
+++ OpenGLContext/__init__.py
@@ -32,7 +32,7 @@ It is also the primary suite of test cases PyOpenGL is verified against.
 Documentation is under `docs/`, indexed by `docs/documentation.html`.
 '''
 
-__version__ = "3.0.0a3"
+__version__ = "3.0.0a4"
 __author__ = "Michael Colin Fletcher"
 __license__ = "BSD-Style, see license.txt for details and exceptions"
 
